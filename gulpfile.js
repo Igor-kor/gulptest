130c20788f4c9d7d0b2ae6f205545db6a8a22dfb
ikor <igor_korenovsk@mail.ru>
2019-12-07 14:45:11 +0300

    настроил иерархию папок

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,38 @@ var browserSync = require('browser-sync');
 gulp.task('sass', function () {
     return gulp.src('./src/sass/**/*.scss')
 .pipe(sass().on('error', sass.logError))
-.pipe(gulp.dest('./'))
+.pipe(gulp.dest('./public/css/'))
 });
 
 gulp.task('watch', function () {
     gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
-    gulp.watch('./**/*.twig', gulp.series('twig'));
+    gulp.watch('./src/twig/**/*.twig', gulp.series('twig'));
 });
 
 
 
 gulp.task('browser-sync', function () {
     var files = [
-        './*.html',
-        './**/*.css',
-        './**/*.png',
-        './**/*.js'
+        './public/index.html',
+        './public/**/*.css',
+        './public/**/*.png',
+        './public/**/*.js'
     ];
 
     browserSync.init(files, {
         server: {
-            baseDir: './'
+            baseDir: './public/'
         }
     });
 });
 
 gulp.task('twig', function () {
-    return gulp.src(['*.twig'])
+    return gulp.src(['./src/twig/index.twig'])
     // Stay live and reload on error
         .pipe(twig())
         .on('error', function (err) {
             process.stderr.write(err.message + '\n');
             this.emit('end');
         })
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./public/'));
 });
